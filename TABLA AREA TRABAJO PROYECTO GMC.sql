ba4bd56216fa6ef2e3640aaf1760a6ec9d281913
willmov <wmoreno_84@outlook.com>
2025-05-25 20:52:49 -0500

    se borra linea
    adasdsa

--- TABLA AREA TRABAJO PROYECTO GMC.sql
+++ TABLA AREA TRABAJO PROYECTO GMC.sql
@@ -1,7 +1,6 @@
 create table AREA_DE_TRABAJO(
 EMAIL_CORPORATIVO varchar(50) primary key,
 OPERATIVO varchar(25),
-TALLER varchar(25),
 MAQUINARIA varchar(25),
 ADMON varchar(25),
 TURNO_TRABAJO varchar(25),
